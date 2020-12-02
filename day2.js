74047dbde5d85cc203304ad10d19ec4057c1e420
rachelfranklin1 <rachel.franklin@marvelapp.com>
2020-12-02 22:40:37 +0000

    I saved the day by getting all of the valid passwords

--- day2.js
+++ day2.js
@@ -1,10 +1,16 @@
+import { day2Input } from "./input";
+
 const acceptedPasswords = day2Input.filter((item) => {
-  const min = item[0];
-  const max = item[1];
+  const positionA = item[0];
+  const positionB = item[1];
   const letterToMatch = item[2];
-  const regexToMatch = new RegExp(letterToMatch, "g");
-  const testString = item[3];
-  const matchedInstancesOfLetters = testString.match(regexToMatch);
-  const length = matchedInstancesOfLetters && matchedInstancesOfLetters.length;
-  return length >= min && length <= max;
+  const stringToSearch = item[3];
+  let itemsMatched = [];
+  if (stringToSearch[positionA - 1] === letterToMatch) {
+    itemsMatched.push(positionA);
+  }
+  if (stringToSearch[positionB - 1] === letterToMatch) {
+    itemsMatched.push(positionB);
+  }
+  return itemsMatched.length === 1;
 });
